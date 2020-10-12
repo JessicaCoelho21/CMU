4197d94250accd060cf8a0b1583804e931cd60d3
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-12 17:43:39 +0100

    Changes in exercise 2 - button added

--- CMU_FP_01/CMU_FP_01_ex2/app/src/main/java/pt/ipp/estg/poolApp/MainActivity.java
+++ CMU_FP_01/CMU_FP_01_ex2/app/src/main/java/pt/ipp/estg/poolApp/MainActivity.java
@@ -3,6 +3,8 @@ package pt.ipp.estg.poolApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -13,17 +15,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        chart();
+
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chart();
+            }
+        });
+    }
+
+    /**
+     * Method that creates a chart with 10 random values
+     */
+    protected void chart(){
         LineChart chart = (LineChart) findViewById(R.id.chart);
         List<Entry> entries = new ArrayList<Entry>();
+
         for (int i =0; i <10; i++) {
             entries.add(new Entry(i, (float) Math.random() ) );
         }
+
         LineDataSet dataSet = new LineDataSet(entries, "");
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
