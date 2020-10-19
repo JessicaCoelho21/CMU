6589170f36b4c32930c04c4d8db30c385571e4f8
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-19 18:28:50 +0100

    Exercise one with problems

--- CMU_FP_02/CMU_FP_02_ex1/app/src/main/java/pt/ipp/estg/activities/MainActivity.java
+++ CMU_FP_02/CMU_FP_02_ex1/app/src/main/java/pt/ipp/estg/activities/MainActivity.java
@@ -2,13 +2,42 @@ package pt.ipp.estg.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mEditText;
+    Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = findViewById(R.id.editText);
+        button1 = findViewById(R.id.button2);
+        button2 = findViewById(R.id.button4);
+
+        Intent i = new Intent(MainActivity.this, NewActivity.class);
+        i.putExtra("Text", (mEditText.getText().toString()));
+        setResult(Activity.RESULT_OK, i);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ CMU_FP_02/CMU_FP_02_ex1/app/src/main/java/pt/ipp/estg/activities/NewActivity.java
@@ -0,0 +1,25 @@
+package pt.ipp.estg.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NewActivity extends AppCompatActivity {
+    String mResultado;
+    TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newactivity);
+
+        resultado = findViewById(R.id.textView);
+        mResultado = getIntent().getStringExtra("Text");
+
+        if(mResultado != null)
+            resultado.setText(mResultado);
+        else
+            resultado.setText("Something went wrong...");
+    }
+}
