b15509534147080956b613d21ad9581629efc8ef
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-23 21:40:19 +0100

    Exercise 3, new layouts added

--- CMU_FP_02/CMU_FP_02_ex3/app/src/main/java/pt/ipp/estg/cmu_fp_02_ex3/MainActivity.java
+++ CMU_FP_02/CMU_FP_02_ex3/app/src/main/java/pt/ipp/estg/cmu_fp_02_ex3/MainActivity.java
@@ -2,6 +2,7 @@ package pt.ipp.estg.cmu_fp_02_ex3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     TextView mResultado;
     EditText mEditText;
     Button mButton;
+    int i = 0;
+    int[] answer = new int[11];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +25,28 @@ public class MainActivity extends AppCompatActivity {
         mEditText = findViewById(R.id.editTextNumber3);
         mButton = findViewById(R.id.button);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Integer.valueOf(mEditText.getText().toString()) == 5) {
-                    mResultado.setText("O Resultado está correto!");
-                } else {
-                    mResultado.setText("O Resultado está errado!");
+        for (i = 0; i < 10; i++) {
+            mButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Integer.valueOf(mEditText.getText().toString()) == 5) {
+                        answer[i] = Integer.valueOf(mEditText.getText().toString());
+                        mResultado.setText("O Resultado está correto!");
+                    } else {
+                        mResultado.setText("O Resultado está errado!");
+                    }
                 }
-            }
-        });
+            });
+        }
+
+        //anotherActivity();
     }
+
+    /*
+    protected void anotherActivity() {
+        Intent in = new Intent(MainActivity.this, NewActivity.class);
+        in.putExtra("Text", (answer));
+        startActivity(in);
+    }
+     */
 }
