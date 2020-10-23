b354335884938f7ea62bf8426dc98e54c28d3099
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-23 20:59:03 +0100

    Beginning of exercise 3

---
+++ CMU_FP_02/CMU_FP_02_ex3/app/src/main/java/pt/ipp/estg/cmu_fp_02_ex3/MainActivity.java
@@ -0,0 +1,36 @@
+package pt.ipp.estg.cmu_fp_02_ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mResultado;
+    EditText mEditText;
+    Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mResultado = findViewById(R.id.textView3);
+        mEditText = findViewById(R.id.editTextNumber3);
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Integer.valueOf(mEditText.getText().toString()) == 5){
+                    mResultado.setText("O Resultado está correto!");
+                } else {
+                    mResultado.setText("O Resultado está errado!");
+                }
+            }
+        });
+    }
+}
