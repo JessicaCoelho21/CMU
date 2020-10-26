730134051d914625403067e6b034d7381c428d03
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-26 12:09:53 +0000

    Added classes' examples

---
+++ Exemplos/IntentExample/app/src/main/java/pt/ipp/estg/intentexample/MainActivity.java
@@ -0,0 +1,30 @@
+package pt.ipp.estg.intentexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button contactos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactos = findViewById(R.id.button);
+
+        contactos.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people/1"));
+        startActivity(intent);
+    }
+}
