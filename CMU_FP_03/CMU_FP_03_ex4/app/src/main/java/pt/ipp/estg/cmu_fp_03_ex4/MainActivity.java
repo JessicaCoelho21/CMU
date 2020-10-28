6b542eb35287c6fa03af3ac6b1d0cf026c9c35e7
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-28 22:18:11 +0000

    Worksheet 3, exercise 4 completed

--- CMU_FP_03/CMU_FP_03_ex4/app/src/main/java/pt/ipp/estg/cmu_fp_03_ex4/MainActivity.java
+++ CMU_FP_03/CMU_FP_03_ex4/app/src/main/java/pt/ipp/estg/cmu_fp_03_ex4/MainActivity.java
@@ -6,10 +6,14 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +21,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button notify;
     private Button update;
     private Button cancel;
+
+    public static final String ACTION_DISABLE = "pt.ipp.estg.DISABLE";
+    public static final String ACTION_UPDATE = "pt.ipp.estg.UPDATE";
+
+    private IntentFilter filter;
+    private MyBroadcastReceiver broadcast;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,38 +43,120 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         update.setEnabled(false);
         cancel.setEnabled(false);
 
+        //Instanciar um receiver para receber intents com filtros/ações específicas
+        broadcast = new MyBroadcastReceiver();
+
+        //Instanciar IntentFilter para adicionar filtros/ações
+        filter = new IntentFilter(ACTION_DISABLE);
+        filter.addAction(ACTION_UPDATE);
+
+        //Registar o receiver com os filtros/ações
+        this.registerReceiver(broadcast, filter);
+
         notify.setOnClickListener(this);
         update.setOnClickListener(this);
         cancel.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
+    protected void onStart() {
+        super.onStart();
+        Log.d("MAIN_ACTIVITY", "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MAIN_ACTIVITY", "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MAIN_ACTIVITY", "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MAIN_ACTIVITY", "onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MAIN_ACTIVITY", "onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.unregisterReceiver(broadcast);
+        Log.d("MAIN_ACTIVITY", "onDestroy()");
+    }
+
+    public void updateNotification() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.transferir);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "id")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Título Alterado!!")
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, mBuilder.build());
+    }
+
+    public void createNotification() {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "id")
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle("Foi notificado!")
                 .setContentText("Este é o texto da notificação")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        //Intent para criar a notificação e abrir a app
+        Intent click = new Intent(MainActivity.this, MainActivity.class);
+        click.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingClick = PendingIntent.getActivity(this, 0, click, 0);
+
+        //Cancela notificação e abre a app
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContentIntent(pendingClick);
+
+        //Intent e PendingIntent com filtro/ação para dar update à notificação
+        Intent updateNotification = new Intent();
+        updateNotification.setAction(ACTION_UPDATE);
+
+        PendingIntent pendingUpdateNotification = PendingIntent.getBroadcast(this, 0, updateNotification, 0);
+
+        mBuilder.addAction(R.drawable.ic_launcher_foreground, "Atualizar notificação", pendingUpdateNotification);
+
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, mBuilder.build());
+    }
 
+    public void deleteNotification() {
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.cancel(1);
+    }
+
+    @Override
+    public void onClick(View v) {
         if (v.getId() == R.id.button) {
-            notificationManager.notify(1, mBuilder.build());
-
+            createNotification();
             update.setEnabled(true);
             cancel.setEnabled(true);
         }
 
         if (v.getId() == R.id.button2) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.transferir);
-
-            mBuilder.setContentTitle("Título Alterado!!");
-            mBuilder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap)).build();
-            notificationManager.notify(1, mBuilder.build());
+            updateNotification();
         }
 
         if (v.getId() == R.id.button3) {
-            notificationManager.cancel(1);
+            deleteNotification();
+            disableButton();
         }
     }
 
@@ -77,5 +170,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+    }
+
+    public void disableButton() {
+        update.setEnabled(false);
+        cancel.setEnabled(false);
     }
 }
